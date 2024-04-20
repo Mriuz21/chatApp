be3669ca6339ea7347555652ab81a27a62e430b3
Mriuz21 <marius.dudui03@e-uvt.ro>
2024-04-20 20:17:21 +0300

    A bit buggy send message from client to server

--- client/client.cpp
+++ client/client.cpp
@@ -4,12 +4,16 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string>
 
-int main() {
+int main()
+{
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
+
+    if(sock == -1)
+    {
         std::cout << "Can't create socket" << std::endl;
         return 1;
     }
@@ -21,11 +25,18 @@ int main() {
     hint.sin_port = htons(port);
     inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
 
-    if (connect(sock, (sockaddr*)&hint, sizeof(sockaddr_in)) == -1) {
-        std::cout << "Can't connect to server" << std::endl;
+    int connectRes = connect(sock, (sockaddr*)&hint, sizeof(sockaddr_in));
+    if(connectRes == -1)
+    {
+        std::cout << "Can't connect" << std::endl;
         return 1;
     }
-
-
+    char message[4096];
+    while(true)
+    {
+        std :: cout << "Write message : ";
+        std :: cin >> message;
+        send(sock, message, strlen(message), 0);
+    }
     return 0;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -14,7 +14,7 @@ int main() {
         return 1;
     }
 
-    int port = 5555;
+    int port = 5555; 
     std::string ipAddress = "127.0.0.1";
     sockaddr_in hint;
     hint.sin_family = AF_INET;
@@ -32,26 +32,47 @@ int main() {
     }
     std::cout << "Listening on " << ipAddress << ":" << port << std::endl;
 
-    sockaddr_in client;
-    socklen_t clientSize = sizeof(client);
-    int newSock = accept(sock, (sockaddr*)&client, &clientSize);
-    if (newSock == -1) {
-        std::cout << "Problem with accept" << std::endl;
-        return 1;
+    while (true) { // Loop to continuously accept connections
+        sockaddr_in client;
+        socklen_t clientSize = sizeof(client);
+        int newSock = accept(sock, (sockaddr*)&client, &clientSize);
+        if (newSock == -1) {
+            std::cout << "Problem with accept" << std::endl;
+            continue; // Continue to accept other connections
+        }
+
+        char host[NI_MAXHOST];
+        char service[NI_MAXSERV];
+        memset(host, 0, NI_MAXHOST);
+        memset(service, 0, NI_MAXSERV);
+
+        if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
+            std::cout << host << " connected on port " << service << std::endl;
+        } else {
+            inet_ntop(AF_INET, &(client.sin_addr), host, NI_MAXHOST);
+            std::cout << host << " connected on port " << ntohs(client.sin_port) << std::endl;
+        }
+
+        // Handle messages from client
+        while (true) {
+            char buffer[4096];
+            memset(buffer, 0, 4096);
+            int bytesReceived = recv(newSock, buffer, 4096, 0);
+            if (bytesReceived == -1) {
+                std::cout << "Error in recv()" << std::endl;
+                break;
+            } else if (bytesReceived == 0) {
+                std::cout << "Client disconnected" << std::endl;
+                break;
+            }
+            std::cout << "Received: " << buffer << std::endl;
+
+            send(newSock, buffer, bytesReceived, 0);
+        }
+
+        // Close the socket when the client disconnects
+        close(newSock);
     }
-
-    char host[NI_MAXHOST];
-    char service[NI_MAXSERV];
-    memset(host, 0, NI_MAXHOST);
-    memset(service, 0, NI_MAXSERV);
-
-    if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
-        std::cout << host << " connected on port " << service << std::endl;
-    } else {
-        inet_ntop(AF_INET, &(client.sin_addr), host, NI_MAXHOST);
-        std::cout << host << " connected on port " << ntohs(client.sin_port) << std::endl;
-    }
-
 
     return 0;
 }
